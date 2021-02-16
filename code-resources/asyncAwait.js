2a3cbe1b3ec0dd9ac8d10c7bdc8233e14ffd31f5
shadreza <shadreza100@gmail.com>
2021-02-16 13:50:41 +0600

    Added Surah Loader ... Have to work on audio , translation , beautification

---
+++ code-resources/asyncAwait.js
@@ -0,0 +1,66 @@
+let t0 = performance.now();
+const surahGallery = document.getElementById('surahGalleryId');
+const surahLoader = document.getElementById('surahLoaderSectionId');
+let theEntireQuranWithArabicTextAndAudio ;
+let theEntireQuranWithEnglishTranslation ;
+let theEntireQuranWithAudio ;
+async function callFromApi (passedUrl) {
+    const response = await fetch(passedUrl);
+    const jsonResponse = await response.json();
+    return jsonResponse.data.surahs;
+}
+async function dataIsLoaded () {
+    console.log(theEntireQuranWithArabicTextAndAudio);
+    console.log(theEntireQuranWithEnglishTranslation);
+    let t1 = performance.now();
+    console.log((t1-t0)/1000 + 'sec');
+}
+function loadInitialContent () {
+    surahGallery.innerHTML='';
+    theEntireQuranWithArabicTextAndAudio.forEach(surahContent => {
+        let individualSurahDiv = document.createElement('div');
+        individualSurahDiv.innerHTML = `
+            <strong>${surahContent.number}</strong>
+            <small>Surah</small>
+            <h4 class='nameOfTheSurah d-flex justify-content-between'>
+                <span>${surahContent.englishName}</span>
+                <span class='arabicNameOfTheSurah'>${surahContent.name}</span>    
+            </h4>
+            <h6>${surahContent.englishNameTranslation}</h6>
+            <h6>${surahContent.revelationType}</h6>
+        `;
+        surahGallery.appendChild(individualSurahDiv);
+        individualSurahDiv.addEventListener('click', () => {
+            surahGallery.style.display='none';
+            surahLoader.innerHTML='';
+            let surahToShowAyahsDiv = document.createElement('div');
+            let surahToShowHederDiv = document.createElement('div');
+            surahToShowHederDiv.innerHTML=`
+                <h3>${surahContent.name}</h3>
+                <h4>${surahContent.englishName}</h4>
+            `;
+            surahToShowHederDiv.style.textAlign='center';
+            surahToShowHederDiv.style.marginBottom='4rem';
+            surahLoader.appendChild(surahToShowHederDiv);
+            surahContent.ayahs.forEach(ayah => {
+                let currentAyahDiv = document.createElement('div');
+                currentAyahDiv.innerHTML=`
+                    <h3>${ayah.text}</h3>
+                `;
+                currentAyahDiv.style.textAlign='right';
+                currentAyahDiv.style.margin='2rem 0';
+                surahToShowAyahsDiv.appendChild(currentAyahDiv);
+            });
+            surahLoader.appendChild(surahToShowAyahsDiv);
+            surahLoader.style.padding='5rem'
+            surahLoader.style.display='block';
+        })
+    });
+}
+async function loadAllSurahs () {
+    theEntireQuranWithArabicTextAndAudio = await callFromApi('http://api.alquran.cloud/v1/quran/ar.alafasy');
+    loadInitialContent();
+    theEntireQuranWithEnglishTranslation = await callFromApi('http://api.alquran.cloud/v1/quran/en.asad');
+    dataIsLoaded();
+}
+loadAllSurahs();
--- code-resources/index.js
+++ code-resources/index.js
@@ -1,83 +1,14 @@
 let couldDataBeFetched = false;
 let theEntireQuranInAnArray = new Array();
-const AyahOfSurahArabicClass = class{
-    hizbQuarterOfTheAyah = 0;
-    juzOfTheAyah = 0;
-    manzilOfTheAyah = 0;
-    numberOfTheAyahInTheEntireQuran = 0;
-    numberOfTheAyahInTheSurah = 0;
-    pageWhereTheAyahIsLocated = 0;
-    rukuWhereTheAyahIsLocated = 0;
-    sajdaNeededForTheAyah = false;
-    arabicVerse = '-';
-    englishTranslationOfTheVerse = '-';
-    audioRecitationOfTheVerse ='-';
-    constructor(hizbQuarter=0 , juz=0 , manzil=0 , positionOfTheAyahInTheQuran=0 , positionOfTheAyahInTheSurah=0 , pageNumber=0 , rukuNumber=0 , sajda=0 , engTranslatedVerse='-'){
-        this.hizbQuarterOfTheAyah = hizbQuarter;
-        this.juzOfTheAyah = juz;
-        this.manzilOfTheAyah = manzil;
-        this.numberOfTheAyahInTheEntireQuran = positionOfTheAyahInTheQuran;
-        this.numberOfTheAyahInTheSurah = positionOfTheAyahInTheSurah;
-        this.pageWhereTheAyahIsLocated = pageNumber;
-        this.rukuWhereTheAyahIsLocated = rukuNumber;
-        this.sajdaNeededForTheAyah = sajda;
-        this.englishTranslationOfTheVerse = engTranslatedVerse;
-        // this.arabicVerse = fetchFromAlQuranDotApi(`http://api.alquran.cloud/v1/ayah/${positionOfTheAyahInTheQuran}`,2);
-        // this.audioRecitationOfTheVerse = fetchFromAlQuranDotApi(`http://api.alquran.cloud/v1/ayah/${positionOfTheAyahInTheQuran}/ar.alafasy`,3);
-    }
-}
-const SurahClass = class {
-    nameOfTheSurahInArabic = '-';
-    nameOfTheSurahInEnglish = '-';
-    englishNameTranslationOfTheSurah = '-';
-    placeOfRevelation = '-';
-    positionOfTheSurahInTheQuran = 0;
-    numberOfAyahsOfTheSurah = 0;
-    ayahsArrayOfTheSurah = new Array();
-    constructor(nameInArabic='-' , nameInEnglish='-' , englishNameTranslation='-' , placeWhereRevealed='-' ,  positionInTheQuran=0 , numberOfAyahs=0 , ayahsOfTheSurah){
-        this.nameOfTheSurahInArabic = nameInArabic;
-        this.nameOfTheSurahInEnglish = nameInEnglish;
-        this.englishNameTranslationOfTheSurah = englishNameTranslation;
-        this.placeOfRevelation=placeWhereRevealed;
-        this.positionOfTheSurahInTheQuran = positionInTheQuran;
-        this.numberOfAyahsOfTheSurah = numberOfAyahs;
-        ayahsOfTheSurah.forEach(ayah => {
-            const currentAyah = new AyahOfSurahArabicClass(ayah.hizbQuarter , ayah.juz , ayah.manzil , ayah.number , ayah.numberInSurah , ayah.page , ayah.ruku , ayah.sajda , ayah.text);
-            this.ayahsArrayOfTheSurah.push(currentAyah);
-        });
-    }
-}
-const fetchFromAlQuranDotApi = (passedFetchingUrl='' , decisionMaker=0) => {
-    couldDataBeFetched = false;
+
+async function getJsonDataFromApiByFetching (passedFetchingUrl) {
     try{
-        const request = new XMLHttpRequest();
-        request.open('GET', passedFetchingUrl, false);  // `false` makes the request synchronous
-        request.send(null);
-        if (request.status === 200) {
-            const jsonInformation = JSON.parse(request.response);
-            if(decisionMaker === 1){
-                const entireInformationOfTheQuran = jsonInformation.data.surahs;
-                entireInformationOfTheQuran.forEach(surah => {
-                    const currentSurah = new SurahClass(surah.name , surah.englishName , surah.englishNameTranslation , surah.revelationType , surah.number , surah.ayahs.length , surah.ayahs);
-                    theEntireQuranInAnArray.push(currentSurah);
-                });
-            }
-            else if(decisionMaker === 2){
-                console.log(jsonInformation);
-            }
-            else if(decisionMaker === 3){
-                console.log(jsonInformation);
-            }
-            couldDataBeFetched = true;
-        }
-        else{
-            couldDataBeFetched = false;
-        }
+        return (await fetch(passedFetchingUrl)).json();
     }
-    catch(error){
+    catch (error) {
         const isOnlineOrOffline = window.navigator.onLine;
         if(isOnlineOrOffline===false){
-            alert('Check Your Internet Connection')
+            alert('Check Your Internet Connection');
         }
         else if(couldDataBeFetched===false){
             alert('Al Quran Server is Down Now!');
@@ -87,8 +18,32 @@ const fetchFromAlQuranDotApi = (passedFetchingUrl='' , decisionMaker=0) => {
         }
         couldDataBeFetched=false;
         console.log(error);
-        return;
     }
 }
-fetchFromAlQuranDotApi('http://api.alquran.cloud/v1/quran/en.asad',1);
+
+async function fetchFromAlQuranDotApi (passedFetchingUrl='' , decisionMaker=0) {
+    couldDataBeFetched = false;
+    const jsonInformation = await getJsonDataFromApiByFetching(passedFetchingUrl);
+    if(decisionMaker === 1){
+        const entireInformationOfTheQuran = jsonInformation.data.surahs;
+        entireInformationOfTheQuran.forEach(surah => {
+            const currentSurah = new SurahClass(surah.name , surah.englishName , surah.englishNameTranslation , surah.revelationType , surah.number , surah.ayahs.length , surah.ayahs);
+            theEntireQuranInAnArray.push(currentSurah);
+        });
+    }
+    else if(decisionMaker === 2){
+        console.log(jsonInformation);
+    }
+    else if(decisionMaker === 3){
+        console.log(jsonInformation);
+    }
+    couldDataBeFetched = true;
+}
+
+document.addEventListener('DOMContentLoaded' , fetchFromAlQuranDotApi('http://api.alquran.cloud/v1/quran/en.asad',1));
 console.log(theEntireQuranInAnArray);
+// setInterval(console.log(theEntireQuranInAnArray),20000);
+// const addSurahsToSurahGallery = () => {
+//     const surahGallery = document.getElementById('surahGalleryId');
+
+// }
